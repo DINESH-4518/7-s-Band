785db98a0a2eea68655a0222e94113b9ace6773c
Dinesh <dhineshsai511@gmail.com>
2025-02-10 13:59:28 +0530

    Deployed through Git PAges

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ function Contact(){
 
             </dl>
             <p className="end"> 
-                Our band has had the privilege of collaborating with several renowned artists and musicians. 
+                Our band has the privilege of collaborating with several renowned artists and musicians. 
                 From joint performances to co-producing tracks, our journey has been enriched by these partnerships. 
                 If you are an artist or a producer interested in collaborating with us, we would love to hear from you. 
                 If you have want to have performance or have any suggestions please do not hesitate to contact us.
